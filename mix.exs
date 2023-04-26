e90fbd44223830f6241e0e47371d505a9dd247fa
Camber Griffin <cambergriffin@Cambers-MacBook-Pro.local>
2023-04-26 10:38:09 -0400

    removed postgres, asses xmerl dep

--- mix.exs
+++ mix.exs
@@ -48,7 +48,8 @@ defmodule AnsiblePhoenix.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:sweet_xml, "~> 0.7.3"}
     ]
   end
 
